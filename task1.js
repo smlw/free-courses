e5544f306fd88054d9346692164ddc12d239f192
Alexander <smlwmy@gmail.com>
2018-06-03 23:12:53 +0500

    Сделал кодирование чисел в СС > 10

--- task1.js
+++ task1.js
@@ -3,6 +3,9 @@
 /* Put your code here */
 
 (function () {
+    var alphabet = [[10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32],
+                    ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+                    ];
 
     // Соберем данные
     var i = process.argv[2];
@@ -16,8 +19,13 @@
     var m = 0;
     while(x > 0){
         m = ~~(x%b); // остаток
-        x = ~~(x/b);
+        x = ~~(x/b); // Целая часть
+
+        m = m >= 10 ? alphabet[1][alphabet[0].indexOf(m)] : m;
         console.log(m);
     }
 
 })();
+
+
+
