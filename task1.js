28ff99128e42c1768d58b4bc070e3219772ed09e
Alexander <smlwmy@gmail.com>
2018-06-04 01:24:32 +0500

    Оптимизировал кодирование чисел > 10

--- task1.js
+++ task1.js
@@ -3,9 +3,7 @@
 /* Put your code here */
 
 (function () {
-    var alphabet = [[10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32],
-                    ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-                    ];
+    var alpha = 'abcdefghijklmnopqrstuvwxyz';
 
     // Соберем данные
     var i = process.argv[2];
@@ -16,14 +14,18 @@
     var x = parseInt(i, a);
 
     // Теперь переведем из 10й в любую другую
-    var m = 0;
+    var m;
     while(x > 0){
         m = ~~(x%b); // остаток
         x = ~~(x/b); // Целая часть
 
-        m = m >= 10 ? alphabet[1][alphabet[0].indexOf(m)] : m;
-        console.log(m);
+
+        m = m >= 10 ? alpha[m-10] : m.toString();
+
+        console.log(m, typeof(m));
     }
+
+    console.log();
 
 })();
 
