c080b02f1e212114b01f58f664f3bd9f0ca22075
Alexander <smlwmy@gmail.com>
2018-06-04 23:22:27 +0500

    Полностью переписал алгоритм

--- task3.js
+++ task3.js
@@ -4,7 +4,7 @@
 var t1 = +process.argv[2];
 var t2 = +process.argv[3];
 
-if (t1 >= 1 && t2 <= 100000) {
+if ((t1 >= 1 && t2 <= 100000) && !(t2 + t1 < 0)) {
     // Summarize
     var sum = t1 + t2;
 
@@ -13,26 +13,31 @@ if (t1 >= 1 && t2 <= 100000) {
     var m = ~~(sum / 60 % 60);
     var s = ~~(sum % 60);
 
+
     // Function for case
     function declOfNum(number, titles) {
         var cases = [2, 0, 1, 1, 1, 2];
         return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
     }
 
-    // Generate title
-    var hTitle = declOfNum(h, [' час', ' часа', ' часов']);
-    var mTitle = declOfNum(m, [' минута', ' минуты', ' минут']);
-    var sTitle = declOfNum(s, [' секунда', ' секунды', ' секунд']);
-
-    // Output results
-    var res;
-    if (h > 0) {
-        res = ((h + hTitle) + ' ' + (m + mTitle) + ' ' + (s + sTitle)).toString();
-    } else if (m > 0) {
-        res = ((m + mTitle) + ' ' + (s + sTitle)).toString();
-    } else {
-        res = (s + sTitle).toString();
+    var res = [];
+    if(h > 0){
+        var hTitle = declOfNum(h, ['час', 'часа', 'часов']);
+        res.push(h, hTitle);
+    }
+    if(m > 0){
+        var mTitle = declOfNum(m, ['минута', 'минуты', 'минут']);
+        res.push(m, mTitle);
+    }
+    if(s > 0){
+        var sTitle = declOfNum(s, ['секунда', 'секунды', 'секунд']);
+        res.push(s, sTitle);
     }
 
+    res = res.join(' ');
+
     process.stdout.write(res);
+
+} else {
+    process.stdout.write('Error')
 }
