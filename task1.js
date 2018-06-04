0c8c73e6b0967377246ae52bf1ac36aca660a0d0
Alexander <smlwmy@gmail.com>
2018-06-04 20:03:56 +0500

    Comment full code task 1

--- task1.js
+++ task1.js
@@ -1,43 +1,43 @@
-/* Task 1 */
-
-/* Put your code here */
-
-(function () {
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-    // Соберем данные
-    var i = process.argv[2];
-    var a = +process.argv[3];
-    var b = +process.argv[4];
-
-    if((i <= 10000 && i >= 0) && (a >= 2) && (b <= 36 && b >= 2)){
-        // Переведем сначала в 10ю СС
-        var x = parseInt(i, a);
-
-        // Теперь переведем из 10й в любую другую
-        var m;
-        var result = [];
-        while(x > 0){
-            m = ~~(x%b); // остаток
-            x = ~~(x/b); // Целая часть
-
-            // Кодирование чисел
-            m = m >= 10 ? alphabet[m-10] : m.toString();
-
-            // пушим в массив
-            result.push(m);
-        }
-
-        result = result.reverse().join('');
-
-        process.stdout.write(result);
-    } else {
-        console.log('Error')
-    }
-
-
-
-})();
-
-
-
+// /* Task 1 */
+//
+// /* Put your code here */
+//
+// (function () {
+//     var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+//
+//     // Соберем данные
+//     var i = process.argv[2];
+//     var a = +process.argv[3];
+//     var b = +process.argv[4];
+//
+//     if((i <= 10000 && i >= 0) && (a >= 2) && (b <= 36 && b >= 2)){
+//         // Переведем сначала в 10ю СС
+//         var x = parseInt(i, a);
+//
+//         // Теперь переведем из 10й в любую другую
+//         var m;
+//         var result = [];
+//         while(x > 0){
+//             m = ~~(x%b); // остаток
+//             x = ~~(x/b); // Целая часть
+//
+//             // Кодирование чисел
+//             m = m >= 10 ? alphabet[m-10] : m.toString();
+//
+//             // пушим в массив
+//             result.push(m);
+//         }
+//
+//         result = result.reverse().join('');
+//
+//         process.stdout.write(result);
+//     } else {
+//         console.log('Error')
+//     }
+//
+//
+//
+// })();
+//
+//
+//
