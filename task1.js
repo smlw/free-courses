9a3701ee425658d7662eec3a7319f96901dc3613
Alexander <smlwmy@gmail.com>
2018-06-05 09:42:47 +0500

    comment full code

--- task1.js
+++ task1.js
@@ -1,40 +1,40 @@
-/* Task 1 */
-
-/* Put your code here */
-
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-// Соберем данные
-var i = process.argv[2];
-var a = +process.argv[3];
-var b = +process.argv[4];
-
-if((i <= 10000 && i >= 0) && (a >= 2 && a <= 32) && (b <= 32 && b >= 2)){
-    // Переведем сначала в 10ю СС
-    var x = parseInt(i, a);
-
-    // Теперь переведем из 10й в любую другую
-    var m;
-    var result = [];
-    while(x > 0){
-        m = ~~(x%b); // остаток
-        x = ~~(x/b); // Целая часть
-
-        // Кодирование чисел
-        m = m >= 10 ? alphabet[m-10] : m.toString();
-
-        // пушим в массив
-        result.push(m);
-    }
-
-    result = result.reverse().join('').toString();
-
-    process.stdout.write(result);
-}
-
-
-
-
-
-
-
+// /* Task 1 */
+//
+// /* Put your code here */
+//
+// var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+//
+// // Соберем данные
+// var i = process.argv[2];
+// var a = +process.argv[3];
+// var b = +process.argv[4];
+//
+// if((i <= 10000 && i >= 0) && (a >= 2 && a <= 32) && (b <= 32 && b >= 2)){
+//     // Переведем сначала в 10ю СС
+//     var x = parseInt(i, a);
+//
+//     // Теперь переведем из 10й в любую другую
+//     var m;
+//     var result = [];
+//     while(x > 0){
+//         m = ~~(x%b); // остаток
+//         x = ~~(x/b); // Целая часть
+//
+//         // Кодирование чисел
+//         m = m >= 10 ? alphabet[m-10] : m.toString();
+//
+//         // пушим в массив
+//         result.push(m);
+//     }
+//
+//     result = result.reverse().join('').toString();
+//
+//     process.stdout.write(result);
+// }
+//
+//
+//
+//
+//
+//
+//
--- task2.js
+++ task2.js
@@ -1,29 +1,29 @@
-/* Task 2 */
-
-/* Put your code here */
-
-(function () {
-
-    var day = +process.argv[2];
-    var month = process.argv[3];
-    var year = +process.argv[4];
-
-    var months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-    month = months.indexOf(month);
-
-    if(( year <= 2999 && year >= 1) && (day >= 1 && day <= 31)){
-
-        var startDate = new Date(year, month, day);
-        var endDate = new Date("12/31/2018");
-
-        var time = Math.abs(endDate.getTime() - startDate.getTime());
-        var days = Math.ceil(time / (1000 * 3600 * 24)).toString();
-
-        process.stdout.write(days);
-
-    } else {
-        console.log('Error')
-    }
-
-
-})();
+// /* Task 2 */
+//
+// /* Put your code here */
+//
+// (function () {
+//
+//     var day = +process.argv[2];
+//     var month = process.argv[3];
+//     var year = +process.argv[4];
+//
+//     var months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+//     month = months.indexOf(month);
+//
+//     if(( year <= 2999 && year >= 1) && (day >= 1 && day <= 31)){
+//
+//         var startDate = new Date(year, month, day);
+//         var endDate = new Date("12/31/2018");
+//
+//         var time = Math.abs(endDate.getTime() - startDate.getTime());
+//         var days = Math.ceil(time / (1000 * 3600 * 24)).toString();
+//
+//         process.stdout.write(days);
+//
+//     } else {
+//         console.log('Error')
+//     }
+//
+//
+// })();
