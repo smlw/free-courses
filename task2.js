25ecb60743cf732c0b9d01c3dd11461a5a90ecb4
Alexander <smlwmy@gmail.com>
2018-06-06 10:09:03 +0500

    Сделал проверку на валидность

--- task2.js
+++ task2.js
@@ -8,25 +8,37 @@ var month = process.argv[3];
 var year = +process.argv[4];
 
 var months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-month = months.indexOf(month);
+month = months.indexOf(month) + 1;
 
-// if(( year <= 2999 && year >= 1) && (day >= 1 && day <= 31)){
+if(( year <= 2999 && year >= 1) && (day >= 1 && day <= 31)){
 
     function f(x,y) {
         return 28 + (x + Math.floor(x / 8)) % 2 + 2 % x + Math.floor((2 - ((y % 4) * ((y % 100) + (y % 400)) + 2) % ((y % 4) * ((y % 100) + (y % 400)) + 1)) / x) + Math.floor(1/x) - Math.floor((1 - ((y % 4) * ((y % 100) + (y % 400)) + 2) % ((y % 4) * ((y % 100) + (y % 400)) + 1))/x);
     }
 
+    // func to get Days
+    function getDays() {
+        var total = 0;
+        for(var i = month; i <= 12; i++){
+            var daysInMonth = f(i,year);
+            total = total + daysInMonth;
+        }
+        total = total - day;
 
-    var total = 0;
-    for(var i = month + 1; i <= 12; i++){
-        var daysInMonth = f(i,year);
-        total = total + daysInMonth;
+        return total.toString();
     }
-    total = total - day;
-    console.log(total);
 
 
-
-
-
-// }
+    // Check valid days
+    var res;
+    if(day >= 28){
+        var dayInMonth = f(month,year);
+        if(day <= dayInMonth){
+            res = getDays();
+            process.stdout.write(res);
+        }
+    } else {
+        res = getDays();
+        process.stdout.write(res);
+    }
+}
